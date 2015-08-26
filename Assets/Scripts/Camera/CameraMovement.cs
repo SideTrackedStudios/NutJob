16ecfbacd98d9c8d24882203a14009b7a9c46647
Jacob Millner <jacob.millner@outlook.com>
2015-08-25 21:16:24 -0400

    Ground and camera movement fixed! :sweat_smile:

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -6,6 +6,7 @@ public class CameraMovement : MonoBehaviour {
 	//set these in the unity interface
 	public Transform cameraHolder;
 	public float cameraSpeed;
+	public Transform player;
 	//there are four camera movement types, set them in the unity interface
 	public CameraMovementType cameraMovementType;
 
@@ -25,16 +26,16 @@ public class CameraMovement : MonoBehaviour {
 	{
 		switch (cameraMovementType) {
 		case CameraMovementType.Lerp:
-			cameraHolder.transform.position = Vector3.Lerp (cameraHolder.transform.position, transform.position, Time.deltaTime * cameraSpeed);
+			cameraHolder.transform.position = Vector3.Lerp (cameraHolder.transform.position, player.position, Time.deltaTime * cameraSpeed);
 			break;
 		case CameraMovementType.MoveTowards:
-			cameraHolder.transform.position = Vector3.MoveTowards (cameraHolder.transform.position, transform.position, Time.deltaTime * cameraSpeed);
+			cameraHolder.transform.position = Vector3.MoveTowards (cameraHolder.transform.position, player.position, Time.deltaTime * cameraSpeed);
 			break;
 		case CameraMovementType.AccelDecel:
-			cameraHolder.transform.position = InterpilationLibrary.AccelDecelInterpolation(cameraHolder.position, transform.position, Time.deltaTime * cameraSpeed);
+			cameraHolder.transform.position = InterpilationLibrary.AccelDecelInterpolation(cameraHolder.position, player.position, Time.deltaTime * cameraSpeed);
 			break;
 		case CameraMovementType.Accerleration:
-			cameraHolder.transform.position = InterpilationLibrary.AccelerationInterpolation(cameraHolder.position, transform.position, Time.deltaTime * cameraSpeed, 1);
+			cameraHolder.transform.position = InterpilationLibrary.AccelerationInterpolation(cameraHolder.position, player.position, Time.deltaTime * cameraSpeed, 1);
 			break;
 			
 		}
