56cf7a0e7fe35aac379c3d4e4c6ff1d3df5d5fcf
Chris Coursey <chris.a.coursey@gmail.com>
2015-08-27 00:23:27 -0400

    Added new player sprites and updated code
    I added the player sprite sheet an spliced it....ton of work (each
    sprite has the correct name and size)
    
    I refactored the playerController and updated couple things.  We may
    have to change to GroundCheck again if we want to be able to fall
    through objects.
    
    Added material "Slick" so our player does not stick to vertical surfaces
    when in air and force is applied.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,94 +3,69 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    //set all these variables up in the unity interface
-    public float moveSpeed;
-    public float jumpHeight;
-    public Transform groundCheck;
-    public float groundCheckRadius;
-    public LayerMask whatIsGround;
-    public bool grounded;
-    private bool doubleJumped;
-    private float moveVelocity;
+    public float Speed = 10f;
+    public float JumpVelocity = 10f;
+    public LayerMask playerMask;
+    public bool CanMoveInAir = true;
 
-    private Animator anim;
-    Rigidbody2D rb2D;
-    Renderer ren;
+    Transform myTransform;
+    Transform groundCheck;
+    Rigidbody2D myBody;
 
-
+    bool isGrounded = false;
+    
 
     // Use this for initialization
     void Start()
     {
-        //anim = GetComponent<Animator> ();
-        //rb2D = GetComponent<Rigidbody2D> ();
-        //ren = GetComponent<Renderer> ();
+        myBody = GetComponent<Rigidbody2D>();
+        myTransform = GetComponent<Transform>();
+        groundCheck = GameObject.Find(this.name + "/GroundCheck").transform;
     }
 
     void FixedUpdate()
     {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        isGrounded = Physics2D.Linecast(myTransform.position, groundCheck.position, playerMask);
+
+        //move the bastard
+        Move(Input.GetAxisRaw("Horizontal"));
+        
+        //do we need to jump
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-            For keybindings, check out Edit->Project Settings->Input in Unity.  Currently WASD/Joystick movement and Space/A to jump.
-        */
 
-        if (grounded)
-        {
-            doubleJumped = false;
-        }
-
-        //anim.SetBool ("Grounded", grounded);
-
-        //jumpy jumpy
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            Jump();
-        }
-
-        if (Input.GetButtonDown("Jump") && !grounded && !doubleJumped)
-        {
-            Jump();
-            doubleJumped = true;
-        }
-
-        //set the moveVel to 0 every frame for more control
-        moveVelocity = 0f;
-
-        //move right
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            //GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-            moveVelocity = moveSpeed;
-        }
-
-        //move left
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            //GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-            moveVelocity = -moveSpeed;
-        }
-
-        GetComponent<Rigidbody2D>().velocity = new Vector3(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
-
-        //anim.SetFloat ("Speed", Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x));
-
-        if (GetComponent<Rigidbody2D>().velocity.x > 0)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
     }
 
+    public void Move(float horizonatalInput)
+    {
+        //this is used to keep the player from changing direction while in air.
+        //our default will let them do this
+        if (!CanMoveInAir && !isGrounded)
+            return;
+
+        Vector2 moveVelocity = myBody.velocity;
+        moveVelocity.x = horizonatalInput * Speed;
+        myBody.velocity = moveVelocity;
+    }
+    void flip()
+    {
+        // Switch the way the player is labelled as facing
+     //   facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
     public void Jump()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+        if (isGrounded)
+            myBody.velocity += JumpVelocity * Vector2.up;
     }
 }
