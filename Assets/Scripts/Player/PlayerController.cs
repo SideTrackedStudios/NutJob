9ce50b9a8ff5d01a5910429c0c87d9913efaf0f9
Chris Coursey <chris.a.coursey@gmail.com>
2015-08-27 01:41:11 -0400

    Added Idle and Walk Animations
    Created animations and set up the animator transitions between the two.
    Once the jump animation is implemented we will also need to check
    grounded.
    PlayerController:
    -----------------------------------
    Moved code from FixedUpdate to Update
    
    Updated Flip function

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,10 +7,10 @@ public class PlayerController : MonoBehaviour
     public float JumpVelocity = 10f;
     public LayerMask playerMask;
     public bool CanMoveInAir = true;
-
     Transform myTransform;
     Transform groundCheck;
     Rigidbody2D myBody;
+    Animator myAnimator;
 
     bool isGrounded = false;
     
@@ -21,25 +21,26 @@ public class PlayerController : MonoBehaviour
         myBody = GetComponent<Rigidbody2D>();
         myTransform = GetComponent<Transform>();
         groundCheck = GameObject.Find(this.name + "/GroundCheck").transform;
+        myAnimator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics2D.Linecast(myTransform.position, groundCheck.position, playerMask);
 
-        //move the bastard
-        Move(Input.GetAxisRaw("Horizontal"));
-        
-        //do we need to jump
-        if (Input.GetButtonDown("Jump"))
-            Jump();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.Linecast(myTransform.position, groundCheck.position, playerMask);
 
+        //move the bastard
+        Move(Input.GetAxisRaw("Horizontal"));
+
+        //do we need to jump
+        if (Input.GetButtonDown("Jump"))
+            Jump();
     }
 
     public void Move(float horizonatalInput)
@@ -48,20 +49,28 @@ public class PlayerController : MonoBehaviour
         //our default will let them do this
         if (!CanMoveInAir && !isGrounded)
             return;
-
+        
         Vector2 moveVelocity = myBody.velocity;
+
         moveVelocity.x = horizonatalInput * Speed;
         myBody.velocity = moveVelocity;
-    }
-    void flip()
-    {
-        // Switch the way the player is labelled as facing
-     //   facingRight = !facingRight;
 
+        //update the animator parameter member with my speed
+        myAnimator.SetFloat("speed", Mathf.Abs(horizonatalInput));
+
+        flip(horizonatalInput);
+    }
+    void flip(float horizonatalInput)
+    {
+        //moving right
+        
         // Multiply the player's x local scale by -1
         Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        if ((myBody.velocity.x > 0 && theScale.x < 0) || myBody.velocity.x < 0 && theScale.x > 0)
+        {
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
     public void Jump()
     {
