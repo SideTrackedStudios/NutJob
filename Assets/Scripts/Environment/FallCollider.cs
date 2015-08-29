eef0a0322b05a30d8c4f9774f2d4c6f67617cf7a
Jason Hafer <haferje@gmail.com>
2015-08-29 00:29:57 -0400

    improved Fall Collider
    stops camera from following player and resets level after a minor delay

--- Assets/Scripts/Environment/FallCollider.cs
+++ Assets/Scripts/Environment/FallCollider.cs
@@ -3,11 +3,38 @@ using System.Collections;
 
 public class FallCollider : MonoBehaviour {
 
+	private GameObject player;
+	private GameObject mainCamera;
+	private CameraMovement movement;
+	private CameraMovement.CameraMovementType origCameraType;
+
 	void OnTriggerEnter2D(Collider2D collider)
 	{
-		if (collider.gameObject.tag == "Player"){
-			Application.LoadLevel(Application.loadedLevel);
+		player = GameObject.Find("Player");
+		mainCamera = GameObject.Find("Main Camera");
+		movement = mainCamera.GetComponent<CameraMovement>();
+
+		if (collider.gameObject.Equals(player))
+		{
+			StopCamera ();
+			Invoke ("ReloadLevel", 1.5f);  // delayed method call
 		}
+	}
+
+	private void StopCamera()
+	{
+		// save the current camera movement type
+		origCameraType = movement.cameraMovementType;
+		// temporarily halt camera movement
+		movement.cameraMovementType = CameraMovement.CameraMovementType.Stopped;
+	}
+
+	private void ReloadLevel()
+	{
+		// restore camera movement type
+		movement.cameraMovementType = origCameraType;
+		// reload the current level
+		Application.LoadLevel(Application.loadedLevel);
 	}
 
 }
